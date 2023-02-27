9400f1cc29c6b685f13a8f991f40d5b811cf4a5a
Anastasiya Gorelkina <115838295+AnastasiyaGorelkina@users.noreply.github.com>
2023-02-28 00:17:37 +0200

    fix

--- js/task-06.js
+++ js/task-06.js
@@ -4,9 +4,9 @@ inputRef.addEventListener('blur', (event) => {
     const inputCurrentLength = event.currentTarget.value.length;
 
     if (inputCurrentLength <= inputRef.dataset.length) {
-        inputRef.classList.add('valid');
+        inputRef.classList.toggle('valid');
     } else {
-        inputRef.classList.add('invalid');
+        inputRef.classList.toggle('invalid');
     }  
 });
 
